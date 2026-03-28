965e092d5c2a6afcbd59f569a882640ce94b6fe4
Parth7007 <parthkadoo007@gmail.com>
2026-03-28 15:04:46 +0530

    update requirements.txt

--- frontend_build/src/config/api.js
+++ frontend_build/src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.https://medicinal-plant-analysis.onrender.com.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://medicinal-plant-analysis.onrender.com";
 
 export const API_ROUTES = {
   predict: `${API_BASE_URL}/api/predict/`,
